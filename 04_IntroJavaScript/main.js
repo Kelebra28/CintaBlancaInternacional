bab6de65028c1d1f343de8b349c518a31535c211
Kelebra <ricardokelebra@gmail.com>
2020-03-27 20:25:55 -0600

    Intro a JavaScrit

---
+++ 04_IntroJavaScript/main.js
@@ -0,0 +1,45 @@
+// alert('!!!!!!!!!!DESCARGA Ya!!!!!!!!!!')
+
+
+
+//Valores en variables
+// Numerico
+var numero = 10;
+//String o Texto
+var nombre = 'Ricardo :D';
+//Boolean 
+var verdadero = true; //false
+//Arreglo o Array
+var arreglo =  [];
+//Objeto o object
+var objeto = {};
+
+//opedadores + - * /
+
+var num1 = 150;
+var num2 = 200;
+var suma = num1 + num2
+
+//Salida de datos
+console.log(suma);
+console.log(nombre);
+console.log(verdadero);
+
+
+var saludo = 'Hola desde la consola ';
+//Concatenar 
+
+console.log(saludo + nombre);
+
+//ENTRADA de datos;
+var pregunta =  prompt('Ingresa tu nombre');
+//SALIDA
+console.log(pregunta);
+
+
+//Preguntar el nombre del usuario, el telefono y su correo, mostrarlo en diferentes consolas
+// preguta 1
+// clg 1
+
+// pregunta2
+// clg2
