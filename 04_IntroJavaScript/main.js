e12a0143c51b02c9d8e6300d105aefe83428b1b6
Kelebra <ricardokelebra@gmail.com>
2020-03-30 20:09:19 -0600

    Condicionales

--- 04_IntroJavaScript/main.js
+++ 04_IntroJavaScript/main.js
@@ -21,20 +21,20 @@ var num2 = 200;
 var suma = num1 + num2
 
 //Salida de datos
-console.log(suma);
-console.log(nombre);
-console.log(verdadero);
+// console.log(suma);
+// console.log(nombre);
+// console.log(verdadero);
 
 
 var saludo = 'Hola desde la consola ';
 //Concatenar 
 
-console.log(saludo + nombre);
+// console.log(saludo + nombre);
 
 //ENTRADA de datos;
-var pregunta =  prompt('Ingresa tu nombre');
+// var pregunta =  prompt('Ingresa tu nombre');
 //SALIDA
-console.log(pregunta);
+// console.log(pregunta);
 
 
 //Preguntar el nombre del usuario, el telefono y su correo, mostrarlo en diferentes consolas
@@ -43,3 +43,30 @@ console.log(pregunta);
 
 // pregunta2
 // clg2
+
+
+//--------------------->
+// Pregutar la edad, y en la consola van a mostrar el año en que nacio
+// 21 ->  1998
+
+var anio = 2020;
+
+console.log(anio);
+
+anio = 'Parrafo'
+
+console.log(anio);
+anio = true
+console.log(anio);
+
+//varibale
+let telefono = '5512431147';
+ console.log(telefono);
+telefono = 500
+console.log(telefono);
+
+//variable constante
+const direccion = 'CDMX';
+console.log(direccion);
+//Es un error cambiar el valot
+// direccion = 02480;
